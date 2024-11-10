3f5ea045f5080c29cdb567726c164f3b7e478d22
Tomasz Sądej <tomasz@tbx.pl>
2024-11-10 23:32:21 +0100

    feature: pinchflat

--- tapczan/index.ts
+++ tapczan/index.ts
@@ -2,6 +2,7 @@ import { Namespace } from "@pulumi/kubernetes/core/v1";
 import sabnzbd from './apps/sabnzbd'
 import sonarr from './apps/sonarr'
 import radarr from './apps/radarr'
+import pinchflat from './apps/pinchflat'
 
 export const namespace: Namespace = new Namespace('tapczan', {
     metadata: { name: "tapczan" }
@@ -12,5 +13,6 @@ const downloader = sabnzbd(namespace)
 export const apps = {
     sabnzbd: downloader,
     sonarr: sonarr(namespace, downloader.service),
-    radarr: radarr(namespace, downloader.service)
+    radarr: radarr(namespace, downloader.service),
+    pinchflat: pinchflat(namespace)
 }
