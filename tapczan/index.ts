93640960094fd03a6f9d40ac0876569bf3d17cf7
Tomasz Sądej <tomasz@tbx.pl>
2025-01-16 15:12:30 +0100

    refactor: remove pinchflat

--- tapczan/index.ts
+++ tapczan/index.ts
@@ -1,13 +1,11 @@
 import sabnzbd from './apps/sabnzbd'
 import sonarr from './apps/sonarr'
 import radarr from './apps/radarr'
-import pinchflat from './apps/pinchflat'
 
 const downloader = sabnzbd()
 
 export const apps = {
     sabnzbd: downloader,
     sonarr: sonarr(downloader.service),
-    radarr: radarr(downloader.service),
-    pinchflat: pinchflat()
+    radarr: radarr(downloader.service)
 }
